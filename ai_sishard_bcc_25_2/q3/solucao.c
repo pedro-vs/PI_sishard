1d31189a5adda90e054284f7970374a1f2c741fc
Pedro Henrique Vargas Sepulveda <159027610+pedro-vs@users.noreply.github.com>
2025-10-03 18:13:04 +0000

    PI resp

--- ai_sishard_bcc_25_2/q3/solucao.c
+++ ai_sishard_bcc_25_2/q3/solucao.c
@@ -1,3 +1,20 @@
 // Seu código da funcao1_solucao
 
+int funcao1_solucao(int esi, long edx, long rdi){
+    int eax = 0;
+    if (eax >= esi){
+        return -1;
+    }
+    long rcx = (long)eax;
+
+    while(edx != (rdi +(4*rcx))){
+        eax = eax +1;
+        if (eax >= esi){
+            return -1;
+        }
+    }
+    return eax;
+
+    
+}
 
